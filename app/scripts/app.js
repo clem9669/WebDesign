91aad48b87132ce5489c4470f40691c2b330e6b4
clem9669 <18504086+clem9669@users.noreply.github.com>
2018-05-12 15:25:05 +0200

    Working map

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -54,14 +54,21 @@ function searchcity() {
       var lat = coordinates[0];
       var lng = coordinates[1];
 
-      var mymap = L.map('map').setView([lat, lng], 4);
-      L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiY2xlbTk2NjYiLCJhIjoiY2poMjRnYTNhMDlkMTJ3cDN0MGNwMnE5NCJ9.pwmUJzWhrwdvFHpe3tk40Q', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        id: 'mapbox.streets',
-        accessToken: 'pk.eyJ1IjoiY2xlbTk2NjYiLCJhIjoiY2poMjRnYTNhMDlkMTJ3cDN0MGNwMnE5NCJ9.pwmUJzWhrwdvFHpe3tk40Q'
-      }).addTo(mymap);
-      var marker = L.marker([lat,lng]).addTo(mymap);
+      // var mymap = L.map('map').setView([lat, lng], 4);
+      // L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiY2xlbTk2NjYiLCJhIjoiY2poMjRnYTNhMDlkMTJ3cDN0MGNwMnE5NCJ9.pwmUJzWhrwdvFHpe3tk40Q', {
+      //   maxZoom: 18,
+      //   center: [lat,lng],
+      //   id: 'mapbox.streets',
+      //   accessToken: 'pk.eyJ1IjoiY2xlbTk2NjYiLCJhIjoiY2poMjRnYTNhMDlkMTJ3cDN0MGNwMnE5NCJ9.pwmUJzWhrwdvFHpe3tk40Q'
+      // }).addTo(mymap);
+    mapboxgl.accessToken ='pk.eyJ1IjoiY2xlbTk2NjYiLCJhIjoiY2poMjRnYTNhMDlkMTJ3cDN0MGNwMnE5NCJ9.pwmUJzWhrwdvFHpe3tk40Q';
+    var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/mapbox/streets-v9', // stylesheet location
+    center: coordinates, // starting position [lng, lat]
+    zoom: 11 // starting zoom
+});
+      // var marker = L.marker([lat,lng]).addTo(mymap);
 
 
     } else {
